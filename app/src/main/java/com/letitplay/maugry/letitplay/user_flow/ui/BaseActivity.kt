b8ecb941602e198ea8c9927c0636b60a92a31c73
Dasha Badanina <b.dasha.ya@gmail.com>
2017-12-12 17:06:52 +0500

    add navigation menu

---
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.letitplay.maugry.letitplay.user_flow.ui
+
+/**
+ * Created by Dasha on 12/12/2017.
+ */
---
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/NavigationActivity.kt
@@ -0,0 +1,13 @@
+package com.letitplay.maugry.letitplay.user_flow.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.letitplay.maugry.letitplay.R
+
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.navigation_main)
+    }
+}
