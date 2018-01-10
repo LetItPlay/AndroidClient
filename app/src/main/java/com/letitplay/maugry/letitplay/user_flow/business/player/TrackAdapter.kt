080110bd307419c1705584d38a9131ee16173c2a
MatkinK <kmatkin@maugry.ru>
2018-01-10 15:01:13 +0500

    Fix bounds of playlist recycler. Change collection type in TrackAdapter #17

--- app/src/main/java/com/letitplay/maugry/letitplay/user_flow/business/player/TrackAdapter.kt
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/business/player/TrackAdapter.kt
@@ -4,25 +4,24 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.letitplay.maugry.letitplay.R
-import com.letitplay.maugry.letitplay.data_management.model.ChannelModel
 import com.letitplay.maugry.letitplay.data_management.model.TrackModel
 import com.letitplay.maugry.letitplay.user_flow.business.BaseViewHolder
 import kotlinx.android.synthetic.main.track_item.view.*
 
 class TrackAdapter : RecyclerView.Adapter<TrackAdapter.TrackItemHolder>() {
 
-    private var data: List<ChannelModel> = ArrayList()
+    private var data: List<TrackModel> = ArrayList()
 
     var onClick: (() -> Unit)? = null
 
     override fun onBindViewHolder(holder: TrackItemHolder?, position: Int) {
         holder?.apply {
-//            update(data[position])
+            update(data[position])
             itemView.setOnClickListener { onClick?.invoke() }
         }
     }
 
-    fun setData(channelList: List<ChannelModel>) {
+    fun setData(channelList: List<TrackModel>) {
         channelList.let {
             data = it
             notifyDataSetChanged()
