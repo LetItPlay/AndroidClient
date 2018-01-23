28195f9ff14c70cea3db3b97c4a5ba20ba12682b
MatkinK <kmatkin@maugry.ru>
2018-01-23 17:43:31 +0500

    Rearrange trend items

--- app/src/main/java/com/letitplay/maugry/letitplay/user_flow/business/trends/TrendsPresenter.kt
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/business/trends/TrendsPresenter.kt
@@ -11,6 +11,7 @@ import com.letitplay.maugry.letitplay.user_flow.business.ExecutionConfig
 import com.letitplay.maugry.letitplay.user_flow.business.Splash.SplashPresenter
 import com.letitplay.maugry.letitplay.user_flow.ui.IMvpView
 import com.letitplay.maugry.letitplay.utils.toAudioTrack
+import java.util.*
 
 object TrendsPresenter : BasePresenter<IMvpView>() {
 
@@ -26,11 +27,15 @@ object TrendsPresenter : BasePresenter<IMvpView>() {
                     asyncObservable = TrackManager.getExtendTrack(),
                     onErrorWithContext = onError,
                     onNextNonContext = {
-                        val sortedTracks = it.sortedByDescending { it.like?.likeCounts }
+                        val now = Date(System.currentTimeMillis())
+                        val sortedTracks = it
                                 .filter {
                                     val lang = it.track?.lang?.let { lang -> ContentLanguage.getLanguage(lang) }
                                     return@filter currentContentLang == lang
                                 }
+                                .takeLastDate(now)
+                                .sortByListenCount()
+
                         extendTrackList = sortedTracks
                         playlist = sortedTracks.map {
                             (it.channel to it.track).toAudioTrack()
@@ -41,15 +46,15 @@ object TrendsPresenter : BasePresenter<IMvpView>() {
     )
 
     fun loadTracksFromRemote(onError: ((IMvpView?, Throwable) -> Unit)? = null, onComplete: ((IMvpView?) -> Unit)? = null) = execute(
-                ExecutionConfig(
-                        asyncObservable = SplashPresenter.allUpdateObservable,
-                        triggerProgress = false,
-                        onErrorWithContext = onError,
-                        onCompleteWithContext = {
-                            loadTracks(false, onError, onComplete)
-                        }
-                )
-        )
+            ExecutionConfig(
+                    asyncObservable = SplashPresenter.allUpdateObservable,
+                    triggerProgress = false,
+                    onErrorWithContext = onError,
+                    onCompleteWithContext = {
+                        loadTracks(false, onError, onComplete)
+                    }
+            )
+    )
 
     fun updateFavouriteTracks(id: Int, body: LikeModel, onComplete: ((IMvpView?) -> Unit)? = null) = execute(
             ExecutionConfig(
@@ -62,4 +67,25 @@ object TrendsPresenter : BasePresenter<IMvpView>() {
             )
 
     )
+
+    private fun List<ExtendTrackModel>.takeLastDate(now: Date): List<ExtendTrackModel> =
+            sortedByDescending {
+                it.track?.publishedAt
+            }.filter {
+                it.track?.publishedAt != null
+            }.takeWhile {
+                val diff = now.time - it.track?.publishedAt?.time!!
+                val diffDate = Date(diff)
+                val cal = Calendar.getInstance()
+                cal.time = diffDate
+                val days = cal.get(Calendar.DAY_OF_MONTH)
+                days < 8
+            }
+
+
+    private fun List<ExtendTrackModel>.sortByListenCount(): List<ExtendTrackModel> =
+            sortedByDescending {
+                it.track?.listenCount
+            }
+
 }
