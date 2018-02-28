e6963873c8f85a2cabc9c632e9cdc694a6e08b65
MatkinK <kmatkin@maugry.ru>
2018-02-28 15:45:21 +0500

    Fix crash in compilations when no internet connection

--- app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/screen/search/compilation/CompilationViewModel.kt
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/screen/search/compilation/CompilationViewModel.kt
@@ -3,6 +3,8 @@ package com.letitplay.maugry.letitplay.user_flow.ui.screen.search.compilation
 import android.arch.lifecycle.ViewModel
 import com.letitplay.maugry.letitplay.data_management.repo.compilation.CompilationRepository
 import com.letitplay.maugry.letitplay.utils.ext.toLiveData
+import io.reactivex.Flowable
+import java.util.concurrent.TimeUnit
 
 
 class CompilationViewModel(
@@ -11,6 +13,7 @@ class CompilationViewModel(
 
     val compilations by lazy {
         compilationRepository.getCompilations()
+                .retryWhen { it.flatMap { Flowable.timer(5, TimeUnit.SECONDS) } }
                 .toFlowable()
                 .toLiveData()
     }
