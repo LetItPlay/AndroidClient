9a4f4d23f0f3e1a7afa994f91214623e4fea645e
MatkinK <kmatkin@maugry.ru>
2018-03-21 10:53:27 +0500

    Bottom sheet dialog with speed options

---
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/widget/PlaybackSpeedDialog.kt
@@ -0,0 +1,85 @@
+package com.letitplay.maugry.letitplay.user_flow.ui.widget
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import com.letitplay.maugry.letitplay.R
+import com.letitplay.maugry.letitplay.data_management.model.DEFAULT_PLAYBACK_SPEED
+import com.letitplay.maugry.letitplay.data_management.model.PlaybackSpeed
+import com.letitplay.maugry.letitplay.user_flow.business.BaseViewHolder
+import kotlinx.android.synthetic.main.playback_speed_item.view.*
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+
+
+class PlaybackSpeedDialog @JvmOverloads constructor(
+        context: Context,
+        attributeSet: AttributeSet? = null,
+        defStyleAttrs: Int = 0
+): FrameLayout(context, attributeSet, defStyleAttrs) {
+    private val recyclerView: RecyclerView
+
+    var onOptionClick: (PlaybackSpeed) -> Unit = {}
+    lateinit var optionAdapter: OptionAdapter
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.playback_speed_dialog, this)
+        recyclerView = findViewById(R.id.options_recycler)
+    }
+
+    fun setItems(options: List<PlaybackSpeed>, currentIndex: Int) {
+        recyclerView.adapter = OptionAdapter(options, currentIndex).also {
+            optionAdapter = it
+        }
+    }
+
+    fun selectAt(index: Int) {
+        optionAdapter.checkedIndex = index
+        optionAdapter.notifyDataSetChanged()
+    }
+
+    inner class OptionAdapter(
+            private val options: List<PlaybackSpeed>,
+            var checkedIndex: Int
+    ): RecyclerView.Adapter<OptionAdapter.OptionViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OptionViewHolder {
+            return OptionViewHolder(parent).apply {
+                itemView.setOnClickListener {
+                    onOptionClick(options[adapterPosition])
+                }
+            }
+        }
+
+        override fun getItemCount(): Int = options.size
+
+        override fun onBindViewHolder(holder: OptionViewHolder, position: Int) {
+            holder.bind(options[position])
+            holder.bindChecked(position == checkedIndex)
+        }
+
+        inner class OptionViewHolder(parent: ViewGroup) : BaseViewHolder(parent, R.layout.playback_speed_item) {
+
+            fun bind(speed: PlaybackSpeed) {
+                val caption = if (speed == DEFAULT_PLAYBACK_SPEED) context.getString(R.string.default_speed) else formatValue(speed.value)
+                itemView.option_caption.text = caption
+            }
+
+            fun bindChecked(value: Boolean) {
+                itemView.option_check.visibility = if (value) View.VISIBLE else View.GONE
+            }
+
+            private fun formatValue(value: Float): String {
+                val df = DecimalFormat("#.##x")
+                val sym = DecimalFormatSymbols.getInstance()
+                sym.decimalSeparator = '.'
+                df.decimalFormatSymbols = sym
+                return df.format(value)
+            }
+        }
+    }
+}
--- app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/widget/PlayerWidget.kt
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/widget/PlayerWidget.kt
@@ -2,28 +2,41 @@ package com.letitplay.maugry.letitplay.user_flow.ui.widget
 
 import android.content.Context
 import android.support.constraint.ConstraintLayout
+import android.support.design.widget.BottomSheetDialog
 import android.support.v4.app.FragmentManager
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import com.gsfoxpro.musicservice.service.MusicService
 import com.letitplay.maugry.letitplay.R
+import com.letitplay.maugry.letitplay.data_management.model.availableSpeeds
 import com.letitplay.maugry.letitplay.user_flow.ui.screen.player.PlayerContainerAdapter
 import kotlinx.android.synthetic.main.player_container_fragment.view.*
 import kotlinx.android.synthetic.main.player_fragment.view.*
 
 
-class PlayerWidget : ConstraintLayout {
-
-    constructor(context: Context) : super(context)
-
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+class PlayerWidget @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
+    : ConstraintLayout(context, attrs, defStyleAttr) {
 
 
     init {
         LayoutInflater.from(context).inflate(R.layout.player_container_fragment, this)
-
+        if (!isInEditMode) {
+            player_playback_button.setOnClickListener {
+                val bottomSheetDialog = BottomSheetDialog(context)
+                val availSpeedList = availableSpeeds()
+                val currentSpeedIndex = 0 // TODO:
+                val optionsDialog = PlaybackSpeedDialog(context).apply {
+                    setItems(availSpeedList, currentSpeedIndex)
+                    onOptionClick = {
+                        // Send command to service
+                        // Change UI
+                        this.selectAt(availSpeedList.indexOf(it))
+                    }
+                }
+                bottomSheetDialog.setContentView(optionsDialog)
+                bottomSheetDialog.show()
+            }
+        }
     }
 
 
