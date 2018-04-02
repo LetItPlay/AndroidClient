f3b5022894c8ccfb65d366d936b36fb86b2489e6
Dasha Badanina <b.dasha.ya@gmail.com>
2018-04-02 11:49:35 +0500

    link

--- app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/screen/player/TrackDetailFragment.kt
+++ app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/screen/player/TrackDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.letitplay.maugry.letitplay.user_flow.ui.screen.player
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.MotionEvent
 import android.view.View
 import com.letitplay.maugry.letitplay.R
@@ -12,6 +13,25 @@ class TrackDetailFragment : BaseFragment(R.layout.track_detail_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        player_track_description.setOnTouchListener(object : View.OnTouchListener {
+            private var touchX = 0f
+            private var touchY = 0f
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        touchX = event.x
+                        touchY = event.y
+                        view.parent.requestDisallowInterceptTouchEvent(false)
+                    }
+
+                    MotionEvent.ACTION_UP -> {
+                        view.parent.requestDisallowInterceptTouchEvent(false)
+                    }
+                }
+                view.onTouchEvent(event)
+                return true
+            }
+        })
         track_detailed_scroll.setOnTouchListener(object : View.OnTouchListener {
             private var touchX = 0f
             private var touchY = 0f
