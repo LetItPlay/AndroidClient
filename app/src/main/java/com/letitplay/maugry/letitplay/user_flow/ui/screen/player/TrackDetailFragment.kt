158beaae8657a53a50fe5f235a5ec03f6040de6f
Dasha Badanina <b.dasha.ya@gmail.com>
2018-04-06 13:56:21 +0500

    PlayerWidget with viewpagerbehaviour

--- app/src/main/java/com/letitplay/maugry/letitplay/user_flow/ui/screen/player/TrackDetailFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.letitplay.maugry.letitplay.user_flow.ui.screen.player
-
-
-import com.letitplay.maugry.letitplay.R
-import com.letitplay.maugry.letitplay.user_flow.ui.BaseFragment
-
-
-class TrackDetailFragment : BaseFragment(R.layout.track_detail_fragment)
