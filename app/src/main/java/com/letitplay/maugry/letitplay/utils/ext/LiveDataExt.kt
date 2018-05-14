560b8d094f0f12ab9b560b5871ef3d174b617770
Dasha Badanina <b.dasha.ya@gmail.com>
2018-05-14 19:02:03 +0500

    follow entity is removed

--- app/src/main/java/com/letitplay/maugry/letitplay/utils/ext/LiveDataExt.kt
+++ app/src/main/java/com/letitplay/maugry/letitplay/utils/ext/LiveDataExt.kt
@@ -6,4 +6,3 @@ import android.arch.lifecycle.MutableLiveData
 import org.reactivestreams.Publisher
 
 fun <T> Publisher<T>.toLiveData() = LiveDataReactiveStreams.fromPublisher(this) as LiveData<T>
-fun <T> Publisher<T>.toMutableLiveData() = LiveDataReactiveStreams.fromPublisher(this) as MutableLiveData<T>
